5f80fb1edd219642b9541da195700b7c890151ef
qoehdgk <coww1996@naver.com>
2020-04-17 14:27:32 +0900

    main.php

--- login_proc.php
+++ login_proc.php
@@ -19,7 +19,7 @@
     session_start();
     $_SESSION['LoginID']=$id;
     echo "<script> alert(\"Login Complete\");";
-    echo "location.href=\"main.html\"</script>";
+    echo "location.href=\"main.php\"</script>";
   }
   else{
     echo "<script> alert(\"Login Fail\");";
---
+++ main.php
@@ -0,0 +1,30 @@
+
+<?php
+  session_start();
+
+  if($_SESSION['LoginID'])
+    echo $_SESSION['LoginID']."님";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Main</title>
+  </head>
+  <body>
+    접속을 환영합니다!! <br>
+    <?php
+      if($_SESSION['LoginID']){
+    ?>
+      <a href="member_modi.php">회원정보 수정</a><br>
+      <a href="member_out.php">회원탈퇴</a><br>
+      <a href="logout.php">로그아웃</a><br>
+    <?php
+      }
+    ?>
+    <a href="board.php">게시판</a><br>
+    <a href="webhard.php">웹하드</a><br>
+  </body>
+</html>
